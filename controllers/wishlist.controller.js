dbb5ce9a4c3c8d1ee1fec609022181c5ebbbe439
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-05-05 09:18:45 +0700

    refactor: get product info in select

--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -3,26 +3,25 @@ import connectToDB from "../config/db.js";
 async function getUserWishList(req, res) {
     const pool = await connectToDB();
     if (!pool)
-        return res
-            .status(500)
-            .json({
-                status: 500,
-                message: "Failed to connect to the database",
-            });
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
 
     try {
         await pool.beginTransaction();
         const user_id = req.params.user_id;
 
         const [rows] = await pool.query(
-            `SELECT * FROM WishList WHERE user_id = '${user_id}'`
+            `SELECT WishList.id, Products.id AS product_id, Products.name AS product_name, Products.price AS product_price, Products.image AS product_image 
+            FROM WishList 
+            INNER JOIN Products ON WishList.product_id = Products.id
+            WHERE user_id = '${user_id}'`
         );
 
         await pool.commit();
 
-        return res
-            .status(200)
-            .json({ status: 200, message: "success", data: rows });
+        return res.status(200).json({ status: 200, message: "success", data: rows });
     } catch (error) {
         await pool.rollback();
 
@@ -35,32 +34,26 @@ async function getUserWishList(req, res) {
 async function addToWishList(req, res) {
     const pool = await connectToDB();
     if (!pool)
-        return res
-            .status(500)
-            .json({
-                status: 500,
-                message: "Failed to connect to the database",
-            });
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
 
     try {
         const { product_id, user_id } = req.body;
 
         await pool.beginTransaction();
 
-        const [result] = await pool.query(
-            "INSERT INTO WishList (product_id, user_id) VALUES (?, ?)",
-            [product_id, user_id]
-        );
+        const [result] = await pool.query("INSERT INTO WishList (product_id, user_id) VALUES (?, ?)", [
+            product_id,
+            user_id,
+        ]);
 
-        const [rows] = await pool.query(
-            `SELECT * FROM WishList WHERE id = '${result.insertId}'`
-        );
+        const [rows] = await pool.query(`SELECT * FROM WishList WHERE id = '${result.insertId}'`);
 
         await pool.commit();
 
-        return res
-            .status(200)
-            .json({ status: 200, message: "success", data: rows[0] });
+        return res.status(200).json({ status: 200, message: "success", data: rows[0] });
     } catch (error) {
         await pool.rollback();
 
@@ -73,24 +66,18 @@ async function addToWishList(req, res) {
 async function deleteItemInWishList(req, res) {
     const pool = await connectToDB();
     if (!pool)
-        return res
-            .status(500)
-            .json({
-                status: 500,
-                message: "Failed to connect to the database",
-            });
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
 
     try {
         const wishlist_item_id = req.params.id;
 
-        const [result] = await pool.query(
-            `DELETE FROM WishList WHERE id = '${wishlist_item_id}'`
-        );
+        const [result] = await pool.query(`DELETE FROM WishList WHERE id = '${wishlist_item_id}'`);
 
         if (result.affectedRows === 0) {
-            return res
-                .status(400)
-                .json({ status: 400, message: "Delete from wishlist failed" });
+            return res.status(400).json({ status: 400, message: "Delete from wishlist failed" });
         }
 
         return res.status(200).json({
