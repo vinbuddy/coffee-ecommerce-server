6ac0e661640244294d47fd04d0e4dbcdacc87154
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-05-23 21:41:31 +0700

    feat: update profile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,19 +3,15 @@ import connectToDB from "../config/db.js";
 async function getUsers(req, res) {
     const pool = await connectToDB();
     if (!pool)
-        return res
-            .status(500)
-            .json({
-                status: 500,
-                message: "Failed to connect to the database",
-            });
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
 
     try {
         const [rows, fields] = await pool.execute("SELECT * FROM Users");
 
-        return res
-            .status(200)
-            .json({ status: 200, message: "success", data: rows });
+        return res.status(200).json({ status: 200, message: "success", data: rows });
     } catch (error) {
         return res.status(500).json({ status: 500, message: error.message });
     } finally {
@@ -27,21 +23,15 @@ async function getUser(req, res) {
     const user_id = req.params.id;
     const pool = await connectToDB();
     if (!pool)
-        return res
-            .status(500)
-            .json({
-                status: 500,
-                message: "Failed to connect to the database",
-            });
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
 
     try {
-        const [rows, fields] = await pool.execute(
-            `SELECT * FROM Users WHERE id = '${user_id}'`
-        );
+        const [rows, fields] = await pool.execute(`SELECT * FROM Users WHERE id = '${user_id}'`);
 
-        return res
-            .status(200)
-            .json({ status: 200, message: "success", data: rows[0] });
+        return res.status(200).json({ status: 200, message: "success", data: rows[0] });
     } catch (error) {
         return res.status(500).json({ status: 500, message: error.message });
     } finally {
@@ -49,4 +39,36 @@ async function getUser(req, res) {
     }
 }
 
-export { getUsers, getUser };
+async function updateProfile(req, res) {
+    const user_id = req.params.id;
+    const pool = await connectToDB();
+    if (!pool)
+        return res.status(500).json({
+            status: 500,
+            message: "Failed to connect to the database",
+        });
+
+    try {
+        const { user_name, avatar } = req.body;
+
+        let sql = `UPDATE Users SET user_name = '${user_name}', avatar = '${avatar}' WHERE id = '${user_id}'`;
+
+        if (user_name && !avatar) {
+            sql = `UPDATE Users SET user_name = '${user_name}' WHERE id = '${user_id}'`;
+        } else if (!user_name && avatar) {
+            sql = `UPDATE Users SET avatar = '${avatar}' WHERE id = '${user_id}'`;
+        }
+
+        await pool.execute(sql);
+
+        const [rows, fields] = await pool.execute(`SELECT * FROM Users WHERE id = '${user_id}'`);
+
+        return res.status(200).json({ status: 200, message: "success", data: rows[0] });
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: error.message });
+    } finally {
+        if (pool) await pool.end();
+    }
+}
+
+export { getUsers, getUser, updateProfile };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUser, getUsers } from "../controllers/user.controller.js";
+import { getUser, getUsers, updateProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 // Query
 router.get("/", getUsers);
 router.get("/:id", getUser);
+router.put("/:id", updateProfile);
 
 export default router;
