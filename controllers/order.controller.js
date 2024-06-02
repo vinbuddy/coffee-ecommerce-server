e8dd13dc213c889126a2c4817a49afe7b142e904
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-06-02 23:17:59 +0700

    refactor: optimize get user orders

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -29,6 +29,98 @@ async function getOrders(req, res) {
     }
 }
 
+// async function getUserOrders(req, res) {
+//     const pool = await connectToDB();
+//     if (!pool)
+//         return res.status(500).json({
+//             status: 500,
+//             message: "Failed to connect to the database",
+//         });
+//     const user_id = req.params.user_id;
+
+//     try {
+//         const [orders] = await pool.query(
+//             `SELECT Orders.*, Users.id AS user_id, Users.user_name, Users.email, Users.avatar, Stores.id AS store_id,
+//             Stores.store_name,
+//             Vouchers.id AS voucher_id,
+//             Vouchers.voucher_name
+//             FROM Orders
+//             LEFT JOIN Users ON Users.id = Orders.user_id
+//             LEFT JOIN Stores ON Stores.id = Orders.store_id
+//             LEFT JOIN Vouchers ON Vouchers.id = Orders.voucher_id
+//             WHERE Orders.user_id = '${user_id}'
+//             ORDER BY Orders.order_date DESC`
+//         );
+
+//         const orderData = [];
+
+//         for (let orderItem of orders) {
+//             const [orderDetails] = await pool.query(
+//                 `SELECT
+//                 od.id,
+//                 p.id AS product_id,
+//                 p.name AS product_name,
+//                 p.price AS product_price,
+//                 p.image AS product_image,
+//                 s.size_name AS size_name,
+//                 ps.size_price AS size_price,
+//                 od.quantity AS quantity
+//                 FROM
+//                     OrderDetails od
+//                 LEFT JOIN
+//                     Products p ON od.product_id = p.id
+//                 LEFT JOIN
+//                     ProductSizes ps ON od.product_id = ps.product_id AND od.size_id = ps.size_id
+//                 LEFT JOIN
+//                     Sizes s ON od.size_id = s.id
+//                 WHERE
+//                     od.order_id = '${orderItem.id}'
+//                 GROUP BY
+//                     od.id, p.name, p.price, p.image, s.size_name, ps.size_price, od.quantity;`
+//             );
+
+//             orderData.push({
+//                 ...orderItem,
+//                 order_items: orderDetails,
+//             });
+//         }
+
+//         for (const orderItem of orderData) {
+//             // Check isReviewed for each order item
+//             const [reviews] = await pool.query(`SELECT * FROM Reviews WHERE order_id = '${orderItem.id}'`);
+//             orderItem["is_reviewed"] = reviews.length > 0;
+
+//             for (const orderDetail of orderItem.order_items) {
+//                 const [orderToppings] = await pool.query(
+//                     `SELECT
+//                     ts.id AS topping_storage_id,
+//                     t.topping_name,
+//                     t.topping_price
+//                     FROM
+//                         ToppingStorages ts
+//                     LEFT JOIN
+//                         Toppings t ON ts.topping_id = t.id
+//                     WHERE
+//                         ts.order_detail_id = '${orderDetail.id}' `
+//                 );
+
+//                 orderDetail["toppings"] = orderToppings.length > 0 ? orderToppings : null;
+//                 orderDetail["order_item_price"] =
+//                     (orderToppings.reduce((acc, curr) => acc + parseFloat(curr.topping_price), 0) +
+//                         parseFloat(orderDetail.product_price) +
+//                         parseFloat(orderDetail.size_price)) *
+//                     parseFloat(orderDetail.quantity);
+//             }
+//         }
+
+//         return res.status(200).json({ status: 200, message: "success", data: orderData });
+//     } catch (error) {
+//         return res.status(500).json({ status: 500, message: error.message });
+//     } finally {
+//         if (pool) await pool.end();
+//     }
+// }
+
 async function getUserOrders(req, res) {
     const pool = await connectToDB();
     if (!pool)
@@ -52,66 +144,83 @@ async function getUserOrders(req, res) {
             ORDER BY Orders.order_date DESC`
         );
 
-        const orderData = [];
+        const orderIds = orders.map((order) => `'${order.id}'`).join(",");
 
-        for (let orderItem of orders) {
-            const [orderDetails] = await pool.query(
-                `SELECT 
-                od.id,
-                p.id AS product_id,
-                p.name AS product_name,
-                p.price AS product_price,
-                p.image AS product_image,
-                s.size_name AS size_name,
-                ps.size_price AS size_price,
-                od.quantity AS quantity
-                FROM 
-                    OrderDetails od
-                LEFT JOIN 
-                    Products p ON od.product_id = p.id
-                LEFT JOIN 
-                    ProductSizes ps ON od.product_id = ps.product_id AND od.size_id = ps.size_id
-                LEFT JOIN 
-                    Sizes s ON od.size_id = s.id
-                WHERE
-                    od.order_id = '${orderItem.id}'
-                GROUP BY
-                    od.id, p.name, p.price, p.image, s.size_name, ps.size_price, od.quantity;`
-            );
+        const [orderDetails] = await pool.query(
+            `SELECT 
+            od.id,
+            od.order_id,
+            p.id AS product_id,
+            p.name AS product_name,
+            p.price AS product_price,
+            p.image AS product_image,
+            s.size_name AS size_name,
+            ps.size_price AS size_price,
+            od.quantity AS quantity
+            FROM 
+                OrderDetails od
+            LEFT JOIN 
+                Products p ON od.product_id = p.id
+            LEFT JOIN 
+                ProductSizes ps ON od.product_id = ps.product_id AND od.size_id = ps.size_id
+            LEFT JOIN 
+                Sizes s ON od.size_id = s.id
+            WHERE
+                od.order_id IN (${orderIds})
+            GROUP BY
+                od.id, p.name, p.price, p.image, s.size_name, ps.size_price, od.quantity;`
+        );
 
-            orderData.push({
-                ...orderItem,
-                order_items: orderDetails,
+        const orderDetailsMap = orderDetails.reduce((map, detail) => {
+            if (!map[detail.order_id]) map[detail.order_id] = [];
+            map[detail.order_id].push(detail);
+            return map;
+        }, {});
+
+        const [reviews] = await pool.query(`SELECT * FROM Reviews WHERE order_id IN (${orderIds})`);
+        const reviewsMap = reviews.reduce((map, review) => {
+            map[review.order_id] = true;
+            return map;
+        }, {});
+
+        const orderDetailIds = orderDetails.map((detail) => `'${detail.id}'`).join(",");
+        const [orderToppings] = await pool.query(
+            `SELECT
+            ts.id AS topping_storage_id,
+            ts.order_detail_id,
+            t.topping_name,
+            t.topping_price
+            FROM
+                ToppingStorages ts
+            LEFT JOIN
+                Toppings t ON ts.topping_id = t.id
+            WHERE
+                ts.order_detail_id IN (${orderDetailIds})`
+        );
+
+        const orderToppingsMap = orderToppings.reduce((map, topping) => {
+            if (!map[topping.order_detail_id]) map[topping.order_detail_id] = [];
+            map[topping.order_detail_id].push(topping);
+            return map;
+        }, {});
+
+        const orderData = orders.map((order) => {
+            const orderItems = orderDetailsMap[order.id] || [];
+            orderItems.forEach((item) => {
+                const toppings = orderToppingsMap[item.id] || [];
+                item.toppings = toppings.length > 0 ? toppings : null;
+                item.order_item_price =
+                    (toppings.reduce((acc, curr) => acc + parseFloat(curr.topping_price), 0) +
+                        parseFloat(item.product_price) +
+                        parseFloat(item.size_price)) *
+                    parseFloat(item.quantity);
             });
-        }
-
-        for (const orderItem of orderData) {
-            // Check isReviewed for each order item
-            const [reviews] = await pool.query(`SELECT * FROM Reviews WHERE order_id = '${orderItem.id}'`);
-            orderItem["is_reviewed"] = reviews.length > 0;
-
-            for (const orderDetail of orderItem.order_items) {
-                const [orderToppings] = await pool.query(
-                    `SELECT
-                    ts.id AS topping_storage_id,
-                    t.topping_name,
-                    t.topping_price
-                    FROM
-                        ToppingStorages ts
-                    LEFT JOIN
-                        Toppings t ON ts.topping_id = t.id
-                    WHERE
-                        ts.order_detail_id = '${orderDetail.id}' `
-                );
-
-                orderDetail["toppings"] = orderToppings.length > 0 ? orderToppings : null;
-                orderDetail["order_item_price"] =
-                    (orderToppings.reduce((acc, curr) => acc + parseFloat(curr.topping_price), 0) +
-                        parseFloat(orderDetail.product_price) +
-                        parseFloat(orderDetail.size_price)) *
-                    parseFloat(orderDetail.quantity);
-            }
-        }
+            return {
+                ...order,
+                order_items: orderItems,
+                is_reviewed: !!reviewsMap[order.id],
+            };
+        });
 
         return res.status(200).json({ status: 200, message: "success", data: orderData });
     } catch (error) {
